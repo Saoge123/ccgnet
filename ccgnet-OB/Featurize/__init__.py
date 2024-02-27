f35069b976a0b48230832e4c23992fd80d9536af
Saoge123 <45249977+Saoge123@users.noreply.github.com>
2024-02-27 11:29:10 +0800

    Delete ccgnet-OB directory

--- ccgnet-OB/Featurize/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Atom_Bond import Atom
-from .CalcuDescriptors import *
-from .Coformer import Coformer
-from .Cocrystal import Cocrystal
-#from .DataSet import make_dataset
-#from .MetricsReport import model_metrics_report
